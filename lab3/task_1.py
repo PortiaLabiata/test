948cc18be60d50225bc6be352a672d26882464fd
PortiaLabiata <filippov.ia@phystech.edu>
2022-10-04 22:19:18 +0300

    Финальный вариант первого задания

--- lab3/task_1.py
+++ lab3/task_1.py
@@ -5,7 +5,16 @@ pygame.init()
 FPS = 30
 screen = pygame.display.set_mode((400, 400))
 
+rect(screen, (255, 255, 255), (0, 0, 400, 400))
+
 circle(screen, (255, 255, 0), (200, 175), 100)
+circle(screen, (255, 0, 0), (150, 135), 25)
+circle(screen, (255, 0, 0), (250, 135), 20)
+circle(screen, (0, 0, 0), (150, 135), 10)
+circle(screen, (0, 0, 0), (250, 135), 10)
+line(screen, (0, 0, 0), [160, 110], [110, 90], 9)
+line(screen, (0, 0, 0), [240, 110], [310, 90], 9)
+line(screen, (0, 0, 0), [165, 210], [235, 210], 20)
 
 pygame.display.update()
 clock = pygame.time.Clock()
