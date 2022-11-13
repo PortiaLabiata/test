82d9cea706aa48ec25ea99bd70c5d2e3fa5e1486
PortiaLabiata <filippov.ia@phystech.edu>
2022-11-13 22:36:04 +0300

    Обработка столкновений скоро будет работать

--- lab7/catch_the_ball3.py
+++ lab7/catch_the_ball3.py
@@ -19,6 +19,7 @@ class Ball(object):
     y = 0
     r = 42
     color = BLACK
+    rect = pygame.Rect(x-r, y-r, x+r, y+r)
     def __init__(self, x_init, y_init, r, color, speed, angle):
         self.x = x_init
         self.y = y_init
@@ -26,6 +27,7 @@ class Ball(object):
         self.color = color
         self.speed = speed
         self.angle = angle
+        self.rect = pygame.Rect(x_init-r, y_init-r, x_init+r, y_init+r)
 
     def move(self, velocity, angle):
         self.x += int(velocity*sin(angle))
@@ -56,11 +58,16 @@ start_rect.center = (xsize // 2, ysize // 2)
 screen.blit(start_surface, start_rect)
 pygame.display.update()
 started = False
+border_top = pygame.Rect(0, 0, xsize, 1)
+border_left = pygame.Rect(0, 0, 1, ysize)
+border_bottom = pygame.Rect(0, ysize, xsize, ysize-1)
+border_right = pygame.Rect(xsize, 0, xsize-1, ysize-1)
 
 while not started:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             started = True
+            finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 started = True
@@ -78,13 +85,19 @@ while not finished:
                     print("Gotcha!")
                     points += 1
                     ball = Ball(randint(20, xsize), randint(20, ysize), randint(10, 50), COLORS[randint(0, 5)], randint(5, 15), randint(0, 180))
+                    points_surface = points_font.render("Score: "+str(points), False, WHITE)
                 else:
                     print("Missed this one(")
                     points -= 1
                     ball = Ball(randint(20, xsize), randint(20, ysize), randint(10, 50), COLORS[randint(0, 5)], randint(5, 15), randint(0, 180))
+                    points_surface = points_font.render("Score: "+str(points), False, WHITE)
 
+
+    if ball.rect.colliderect(border_top) or ball.rect.colliderect(border_bottom) or ball.rect.colliderect(border_left) or ball.rect.colliderect(border_right):
+        ball.angle = 180 - ball.angle
     ball.move(ball.speed, ball.angle)
     screen.fill(BLACK)
+    screen.blit(points_surface, (10, 10))
     ball.draw()
     pygame.display.update()
 
